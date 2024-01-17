8516e30173adc1d32553cf03b13ed4ff9df0d74b
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-01-17 16:02:00 +0200

    Update App.js

--- TASK/back-end/task-front-end/src/App.js
+++ TASK/back-end/task-front-end/src/App.js
@@ -1,46 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import Header from './components/Header';
-import RegistrationForm from './components/RegistrationForm';
-import LoginForm from './components/LoginForm';
-import TaskForm from './components/TaskForm';
-import ToggleBtn from './components/ToggleBtn';
+import React, { useEffect, useState } from 'react';// Import the React module to use React functionalities
+import './App.css';//Import CSS stylesheet
+//Bootstrap
+import Container from 'react-bootstrap/Container';//Import bootstrap container
+import Row from 'react-bootstrap/Row';//Import bootstrap row 
+import Col from 'react-bootstrap/Col';//Import bootstrap coloumn
+import Button from 'react-bootstrap/Button';// Import button component from bootstrap library
+//Components
+import Header from './components/Header';//Import Header function component
+import RegistrationForm from './components/RegistrationForm';//Import RegistrationForm function component
+import LoginForm from './components/LoginForm';//Import LoginForm function component
+import TaskForm from './components/TaskForm';//Import TaskForm Function component
+import ToggleBtn from './components/ToggleBtn';//Import the ToggleBtn component
 
-export default function App() {
-  const [taskData, setTaskData] = useState([]);
-  const [taskInput, setTaskInput] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [newUsername, setNewUsername] = useState(' ');
-  const [newPassword, setNewPassword] = useState('');
+//App Function component
+export default function App() {//Export default App function component
+  //===========STATE VARIABLES=================
+  //Task variables
+  const [taskData, setTaskData] = useState([]);//State to store the array of tasks
+  const [taskInput, setTaskInput] = useState('');//State to store taskInput
+  //User variables
+  const [username, setUsername] = useState('');//State to store the password entered by the user.
+  const [password, setPassword] = useState('');//State to store the password entered by the user.
+  const [newUsername, setNewUsername] = useState(' ');//State to store newUsername
+  const [newPassword, setNewPassword] = useState('');//State to store newPassword
   const [isLoaded, setIsLoaded] = useState(false);
-  const [error, setError] = useState(null);
-  const [login, setLogin] = useState(false);
-  const [loginStatus, setLoginStatus] = useState(true);
-  const [isRegistration, setIsRegistration] = useState(false);
+  //Event variables  
+  const [isLoaded, setIsLoaded] = useState(false);//State to indicate whether the data has loaded
+  const [error, setError] = useState(null);//State to store any error that may occur during data fetching or operations
+  const [login, setLogin] = useState(false);//State to represent the login status
+  const [loginStatus, setLoginStatus] = useState(true);//State used to indicate the login status
+  const [isRegistration, setIsRegistration] = useState(false);//State to indicate whether the user is using the registration form
 
+  //===============USE EFFECT HOOKS==============
+  // useEffect hooks to fetch tasks and initialize login status
   useEffect(() => {
-    const storedTasks = sessionStorage.getItem('tasks');
+    const storedTasks = sessionStorage.getItem('tasks');// Retrieve tasks from session storage using the key 'tasks'
 
+    //Conditional rendering to check if there are tasks stored in sessionStorage
     if (storedTasks) {
+         // If tasks are found, parse the JSON string and update the taskData state 
       setTaskData(JSON.parse(storedTasks));
     }
-  }, []);
+  }, []);//The empty dependency array ([]) means that the useEffect will only run once, after the initial render. 
 
+  //===============REQUESTS=====================
+  //------------GET REQUEST------------------
   useEffect(() => {
     async function fetchTasks() {
       try {
         const token = sessionStorage.getItem('token');
+        // Send a GET request to the server
         const response = await fetch('http://localhost:3001/users/findTasks', {
-          method: 'GET',
-          mode: 'cors',
+          method: 'GET',//Specify the request method
+          mode: 'cors',// Set the mode to 'cors' for cross-origin resource sharing, indicating that the request is a cross-origin request. 
+          /*This is relevant when making requests to a different domain, and it enables the browser to 
+          include the necessary headers for cross-origin communication.*/
           headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',//Specify the Content-Type of the request body
+            'Authorization': `Bearer ${token}`,// Include the Authorization header with the token
           }
         });
 
@@ -60,26 +77,31 @@ export default function App() {
     fetchTasks();
   }, []);
 
+  //============POST REQUEST================
+  //Function to submit login
   const submitLogin = async () => {
     try {
       const response = await fetch('http://localhost:3001/users/login', {
-        method: 'POST',
+        method: 'POST',//Specify the request method
         mode: 'cors',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify({ username, password }),// Convert user credentials to JSON and include in the request body
       });
 
       if (response.status >= 200 && response.status < 300) {
         const data = await response.json();
         console.log('Successfully logged in');
-        setLogin(true);
-        setLoginStatus(true);
-        sessionStorage.setItem('loginStatus', JSON.stringify(true));
-        sessionStorage.setItem('username', username);
-        sessionStorage.setItem('token', data.token);
-      } else {
+        setLogin(true);// Set the 'login' state to true, indicating that the user is logged in
+
+        setLoginStatus(true);// Set the 'loginStatus' state to true, indicating the overall login status
+
+        sessionStorage.setItem('loginStatus', JSON.stringify(true));// Store the login status in sessionStorage as a string
+        sessionStorage.setItem('username', username);// Store the username in sessionStorage
+        sessionStorage.setItem('token', data.token);// Store the authentication token received from the server in sessionStorage
+      } 
+      else {
         throw new Error('Failed to login');
       }
     } catch (error) {
@@ -110,7 +132,7 @@ export default function App() {
     try {
       const token = sessionStorage.getItem('token');
       const response = await fetch('http://localhost:3001/addTask', {
-        method: 'POST',
+        method: 'POST',//Specify the request method
         mode: "cors",
         headers: {
           'Content-type': 'application/json',
