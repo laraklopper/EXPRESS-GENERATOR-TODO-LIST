4b356c19a13b8f48f707b889092902c1e0cbd215
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-02-03 15:04:03 +0200

    Update LogoutBtn.js

--- notes/Examples/front-end/src/components/LogoutBtn.js
+++ notes/Examples/front-end/src/components/LogoutBtn.js
@@ -1,16 +1,26 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React from 'react';// Import the React module to use React functionalities
+import Row from 'react-bootstrap/Row';//Import bootstrap row 
+import Col from 'react-bootstrap/Col';//Import bootstrap coloumn
+import Button from 'react-bootstrap/Button';// Import button component from bootstrap library
 
-export default function LogoutBtn({logout}) {
+//LogoutBtn function component
+export default function LogoutBtn(//Export default LogoutBtn function component
+    {
+       logout
+    }
+) {
+  
+
+    //========JSX RENDERING================
   return (
-      <Row id='logoutRow'>
-          <Col id='logoutCol'>
-              <Button variant="primary" id='logoutBtn' onClick={logout}>
-                  LOGOUT
+      <div id='logout'>  
+        <Row className='row'>
+          <Col className='col'>
+              {/* Button to Logout */}
+              <Button variant="primary" onClick={logout} id='logoutBtn'>
+                  Logout
               </Button>
           </Col>
-      </Row>
+      </Row></div>
   )
 }
