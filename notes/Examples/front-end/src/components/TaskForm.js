e45fae0277cd1bcf574d2572a5d502c0be941833
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-02-03 15:04:40 +0200

    Update TaskForm.js

--- notes/Examples/front-end/src/components/TaskForm.js
+++ notes/Examples/front-end/src/components/TaskForm.js
@@ -1,66 +1,80 @@
-import React from 'react'
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+import React from 'react';// Import the React module to use React functionalities
+import Form from 'react-bootstrap/Form';// Import Form component from bootstrap library
+import Col from 'react-bootstrap/Col';//Import bootstrap coloumn
+import Row from 'react-bootstrap/Row';//Import bootstrap row 
+import Button from 'react-bootstrap/Button';// Import button component from bootstrap library
 
-export default function TaskForm({
-    newTask,
-    setNewTask,
-    addTask
-}) {
+//TaskForm function component
+export default function TaskForm(//Export default TaskForm function component
+    { //=======PROPS PASSED FROM THE PARENT COMPONENT==========
+        addTask, newTask, setNewTask
+    }) {
+        
 
+    //============Event Listeners================
 
+    // Function to handle input changes in the taskForm
     const handleTaskInput = (event) => {
-        const { name, value } = event.target
-        setNewTask((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }))
+        const { name, value } = event.target; // Extract 'name' and 'value' properties from the event target (input element)
+
+        setNewTask((prevData) => ({// Update the 'newTask' state using the setNewTask function
+            ...prevData, // The new state is derived from the previous state (prevData) using the spread operator
+            [name]: value // The property specified by 'name' is updated with the new 'value'
+        }));
     }
-  return (
-      <div id='form'>
-          <Row className='formRow'>
-              <Col className='formCol'>
-                  <h2 className='h2'>ADD TASK</h2>
-              </Col>
-          </Row>
-          <form onSubmit={addTask}>
-              <Row className='formROw'>
-                  <Col xs={6} md={4} className='formCol'>
-                      <label className='formLabel'>
-                          <p className='labelText'>USER:</p>
-                          <input
-                              type='text'
-                              name='username'
-                              value={newTask.username}
-                              onChange={handleTaskInput}
-                              placeholder='username'
-                              autoComplete='on'
-                              className='taskInput'
-                          />
-                      </label>
+
+    //===========JSX RENDERING==================
+
+    return (
+        <div id='form'>
+            <Row className='formRow' >
+                <Col id='formHeader'>
+                    <h3 className='h3'>ADD TASK</h3>
+                </Col>
+            </Row>
+            {/* Form to add new task */}
+             <Form onSubmit={addTask} id='taskForm'>
+                <Row className='formRow'>                
+                    <Col className='formCol'>
+                        {/* Task Input */}
+                       <label className='formLabel'>
+                        <p className='labelText'>USER</p>
+                        <Form.Control
+                            placeholder="username"
+                            type='text'
+                            name='username'
+                            value={newTask.username}
+                            onChange={handleTaskInput}
+                            className='taskInput'
+                            /> 
+                       </label>                            
                   </Col>
-                  <Col xs={6} md={4}>
-                      <label className='formLabel'>
-                          <p className='labelText'>TITLE:</p>
-                          <input
-                              type='text'
-                              name='title'
-                              value={newTask.title}
-                              onChange={handleTaskInput}
-                              placeholder='task'
-                              className='taskInput'
-                              autoComplete='on'
-                          />
-                      </label>
+                  <Col>
+                  <Row>
+                    <Col className=''>
+                    <label className='formLabel'>
+                        <p className='labelText'>TASK:</p>
+                        <Form.Control
+                        placeholder='Task'
+                        type='text'
+                        name='title'
+                        value={newTask.title}
+                        onChange={handleTaskInput}
+                        className='taskInput'
+                        />
+                    </label>
+                    </Col>
+                  </Row>
                   </Col>
-                  <Col xs={6} md={4}>
-                      <Button variant="primary" type="submit" id='addTaskBtn'>
-                          ADD TASK
-                      </Button>
-                  </Col>
-              </Row>
-          </form>
-      </div>
-  )
+                    <Col className='formCol'>
+                    <Button type="submit" variant="primary" id='addTaskBtn'>
+                        ADD TASK
+                    </Button>
+                    </Col>
+                    <Col className='formCol'>
+                    </Col>
+                </Row> 
+            </Form>
+        </div>
+    );
 }
