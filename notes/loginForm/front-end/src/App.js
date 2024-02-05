adc477bb628d7f8c5c8b8a04e670effe96a11d4a
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-02-05 15:08:24 +0200

    Update App.js

--- notes/loginForm/front-end/src/App.js
+++ notes/loginForm/front-end/src/App.js
@@ -7,6 +7,46 @@ const App = () => {
   const [loginStatus, setLoginStatus] = useState(false);
   const [error, setError] = useState('');
 
+  // Function to fetch tasks
+  const fetchTasks = async () => {
+    try {
+      // Check if the authentication token is available
+      if (!token) {
+        setError('Authentication token not found.');
+        return;
+      }
+
+      // Fetch tasks from the server
+      const response = await fetch('http://localhost:3001/users/findTasks', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+
+      // Check if the response is successful (status code between 200 and 299)
+      if (response.ok) {
+        // Parse the response body as JSON
+        const fetchedData = await response.json();
+        
+        
+        setTaskData(fetchedData);
+
+        console.log(fetchedData);
+      } else {
+        // If the response status is not ok, throw an error
+        throw new Error('Failed to fetch tasks');
+      }
+    } catch (error) {
+      setError(`Error fetching data: ${error.message}`);
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  // Call the fetchTasks function when the component mounts or when the token changes
+  fetchTasks();
   const submitLogin = async () => {
     try {
       const response = await fetch('http://localhost:3001/users/login', {
