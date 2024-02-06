b5ff1186750bdefab99546fa45fb6135bfc14fbf
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-02-06 13:04:16 +0200

    Update middleware.js

--- TASK/back-end/routes/middleware.js
+++ TASK/back-end/routes/middleware.js
@@ -50,10 +50,25 @@ const checkJwtToken = (req, res, next) => {
         // If no 'token' header is attached, return a 401 Unauthorized response
         res.status(401).json({ message: 'No token attached to the request' });
     }
+}
+// Middleware function for user authentication
+    const authenticateUser = (req, res, next) => {
+        try {
+            const token = req.headers.authorization.split(' ')[1];        // Extract the token from the Authorization header in the request
+
+            req.body.username = jwt.verify(token, tokenSecret);// Verify the token using the jwt.verify method, and decode the username from it
+
+            next();        // Call the next middleware function or route handler in the chain
+
+        } catch (error) {
+            // If an error occurs during token verification, send a 401 Unauthorized response
+            res.status(401).json({ message: 'Auth failed' });
+        }
+    }
 //Middleware function to check if the provided username is valid
-function validateUsername = (req, res, next) => {
+const validateUsername = (req, res, next) => {
     const username = req.body.newUsername;    // Extract 'username' in the request body or query parameters
-    console.log(username);
+    console.log(username);//Log the username in the console
 
     // Conditional rendering to check if the username ends with '@gmail.com'
     if (username && username.endsWith('@gmail.com')) {
@@ -83,10 +98,12 @@ function validateUsername = (req, res, next) => {
 // };
 
 // Middleware function to limit the length of a task title
-function limitTaskLength(req, res, next) {
-    const { newTask } = req.body;
+const limitTaskLength =(req, res, next) => {
+    const { newTask } = req.body;// Extract the new task title from the request body
     const maxLength = 140;//Max length
 
+
+    
     // Conditional rendering to check if the task title exceeds the maximum length
     if (newTask && newTask.length > maxLength) {
         return res.status(400).json({
@@ -97,7 +114,6 @@ function limitTaskLength(req, res, next) {
     // If the task title length is within the limit, proceed to the next middleware/route
     next();
 }
-
 // Middleware function to enforce the 'Content-Type' header to be 'application/json'
 function enforceContentType(req, res, next) {
     const contentType = req.headers['content-type'];
@@ -122,24 +138,46 @@ function enforceContentType(req, res, next) {
         }
     }
 
-// const authenticate = (req, res, next) => {
-//     if(req.body.username === users.username && req.body.password === users.password){
-//         console.log("user authenticated");
-//         next()
-        
-//     }{
-//         console.error("User Authentication failed")
-//         res.status(401).json({ message: 'Invalid or expired token' });
-        
-//     }
-// }
+//Middleware function to limit the length of an updated task
+    const limitUpdatedTaskLength = (req, res, next) => {
+        const { title } = req.body; // Extract the title from the request body
+        const maxLength = 140;//Max length
+
+        // Conditional rendering to check if the task title exceeds the maximum length
+        if (title && title.length > maxLength) {
+            return res.status(400).json({
+                message: `Task title exceeds the maximum length of ${maxLength} characters.`,
+            });
+        }
+        // If the task title length is within the limit, proceed to the next middleware/route
+        next();
+    }
+
+// Middleware function to enforce the 'Content-Type' header to be 'application/json'
+const enforceContentType= (req, res, next) => {
+    const contentType = req.headers['content-type'];    // Extract the 'Content-Type' header from the request
+
+    // Conditional rendering if 'Content-Type' is present and set to 'application/json'
+    if (!contentType || contentType.indexOf('application/json') !== 0) {
+        // If not, return a 415 Unsupported Media Type response
+        return res.status(415).send('Unsupported Media Type: Content-Type must be application/json');
+    }
+
+  
+    next();// If 'Content-Type' is valid, move to the next middleware or route handler
+}
+
+
 // Export the middleware functions for use in other parts of the application
 module.exports = {
     authenticateToken,// Middleware function to authenticate a JWT token from the 'Authorization' header
     checkJwtToken,// Middleware function to check and verify a JWT token from the 'token' header
     validateUsername,// Middleware function to validate the format of a username
+    authenticateUser,
     limitTaskLength,// Middleware function to limit the length of a task title
+    limitUpdatedTaskLength,//Middleware function to limit the length of an updated task
     enforceContentType,// Middleware function to enforce the 'Content-Type' header to be 'application/json'   
-}}
+}
+
 
 
