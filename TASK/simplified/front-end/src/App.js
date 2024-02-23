4742d7a9ba90d8147b3912c5e5d0858ded264e6a
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-02-23 14:45:44 +0200

    Update App.js

--- TASK/simplified/front-end/src/App.js
+++ TASK/simplified/front-end/src/App.js
@@ -2,17 +2,17 @@
 import React, { useEffect, useState } from 'react'
 import './App.css';//Import CSS File
 //Bootstrap
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';//Import bootstrap container
+import Row from 'react-bootstrap/Row';//Import Bootstrap Row
+import Col from 'react-bootstrap/Col';//Import bootstrap Colomn
+import Button from 'react-bootstrap/Button';//Import bootstrap Button component
 //Components
-import Login from './components/Login';
-import Registration from './components/Registration';
-import AddTask from './components/AddTask';
-import Header from './components/Header';
-import ToggleBtn from './components/ToggleBtn';
-import LogoutBtn from './components/LogoutBtn';
+import Login from './components/Login'; // Import the Login component
+import Registration from './components/Registration'; // Import the Registration component
+import AddTask from './components/AddTask'; // Import the AddTask component
+import Header from './components/Header'; // Import the Header component
+import ToggleBtn from './components/ToggleBtn'; // Import the ToggleBtn component
+import LogoutBtn from './components/LogoutBtn'; // Import the LogoutBtn component
 
 //App function component
 export default function App() {//Export default App function component
@@ -46,25 +46,25 @@ export default function App() {//Export default App function component
         const token = localStorage.getItem('token')// Retrieve token from localStorage
         //Send a GET request to the /findTasks endpoint
         const response = await fetch ('http://localhost:3001/users/findTasks',{
-          method: 'GET',
-          mode: 'cors',
+          method: 'GET',//Request method
+          mode: 'cors',//
           headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json',//Specify the Content_Type being passed
             'Authorization': `Bearer ${token}`
           }
         });
-        
+        //Conditional rendering to check if the response is successful
         if (response.ok) {
-          const fetchedData = await response.json();
-          setTasks(fetchedData)
-          console.log(fetchedData);
+          const fetchedData = await response.json();// Parse the response data as JSON
+          setTasks(fetchedData); // Set the tasks state with the fetched data
+          console.log(fetchedData);//Log the fetched data in the console
         } 
         else {
-          throw new Error('Failed to fetch tasks')
+          throw new Error('Failed to fetch tasks');//Throw an error message if the GET request is unsuccessful
         }
       } 
       catch (error) {
-        setError(`Error fetching data: ${error.message}`);
+        setError(`Error fetching data: ${error.message}`);// Set error state if an error occurs 
       }
     }
     // Invoke the fetchTasks function when the component mounts
@@ -76,114 +76,134 @@ export default function App() {//Export default App function component
   //------------POST REQUESTS--------------------
 
   //Function to submitLogin
-  const submitLogin = async () => {//Define an async function to submitLogin 
-    
-    try {
-      const response =await fetch ('http://localhost:3001/users/login', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            username: userData.username, 
-            password: userData.password
-          })
-      })
+  const submitLogin = async (e) => { // Define an async function to submitLogin 
+  e.preventDefault(); // Prevent the default form submission behavior
 
-      if (response.ok) {
-        const data = await response.json()
-        if (data.token) {
-          localStorage.setItem('username', userData.username);
-          setLoggedIn(true);
-          setLoggedOut(false)
-          localStorage.setItem('token', data.token)
-        } else {
-          throw new Error ('Invalid server response')
-        }  
+  try {
+    // Send a POST request to the login endpoint
+    const response = await fetch('http://localhost:3001/users/login', {
+      method: 'POST',//Request method
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json', // Specify the content type as JSON
+      },
+      body: JSON.stringify({ // Convert the user data to a JSON string
+        username: userData.username, // Get the username from the userData state
+        password: userData.password, // Get the password from the userData state
+      }),
+    });
+
+    //Conditional rendering to check if the response is successful
+    if (response.ok) { 
+      const data = await response.json(); // Parse the response body as JSON
+      //Conditional rendering to check if a token is present in the response data
+      if (data.token) { 
+        // Store the username and token in the local storage
+        localStorage.setItem('username', userData.username);
+        localStorage.setItem('token', data.token);
+        // Update state variables to indicate successful login
+        setLoggedIn(true);
+        setLoggedOut(false);
+        setError(''); // Clear any previous error messages
       } 
       else {
-        throw new Error('Failed to login');
+        throw new Error('Invalid server response');// Throw an error if the server response does not contain a token
       }
-
-    } 
-    catch (error) {
-      setError(`Login Failed ${error.message}`)
-      console.log(`Login Failed ${error.message}`);
+    } else {
+      
+      throw new Error('Username or password are incorrect');// Throw an error if the server response indicates incorrect username or password
     }
+  } catch (error) {
+    // Catch any errors that occur during the login process
+    setError(`Login Failed ${error.message}`); // Set error message in state
+    console.log(`Login Failed ${error.message}`); // Log the error message to the console for debugging purposes
   }
+};
 
   //Function to add a new user
-  const addUser = async (e) => {
-    try {
-      const token = localStorage.getItem('token');
-      if (!token) {
-        throw new Error('No token available');
-      }
-      const response = await fetch ('http://localhost:3001/users/register', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-          newUsername: newUserData.newUsername, 
-          newPassword : newUserData.newPassword
-        })
-      })
+const addUser = async (e) => {//Define an async function to add a new User
+  e.preventDefault(); // Prevent the default form submission behavior
 
-      if (response.ok) {
-        const data = await response.json()
-        if (data.token) {
-          
-          const users = JSON.parse(localStorage.getItem('users')) || [];
-          const newUser = { username: newUserData.newUsername, password: newUserData.newPassword, userId: users.length + 1 };
-          users.push(newUser)
-          localStorage.setItem('users', JSON.stringify(users))
-        }
-        else{
-          throw new Error ('Invalid Server Response')
-        }       
-      } 
-      else 
-      {
-        throw new Error('Failed to add new user');
-      }
-    } 
-    catch (error) {
-      setError(`Error adding new user: ${error.message}`);
-      console.error('Registration Failed:', error.message);
+  try {
+    const token = localStorage.getItem('token'); // Retrieve the token from local storage
+    //Conditional rendering to check if the token is available
+    if (!token) {
+      throw new Error('No token available'); // Throw an error if no token is available
     }
+    
+    // Send a POST request to the register endpoint
+    const response = await fetch('http://localhost:3001/users/register', {
+      method: 'POST',//Request method
+      mode: 'cors',
+      headers: {
+        // 'Content-Type': 'application/json',
+        'Content-Type': 'application/json; charset=UTF-8', // Specify the content type as JSON
+        'Authorization': `Bearer ${token}`, // Include the token in the authorization header
+      },
+      body: JSON.stringify({ // Convert the new user data to a JSON string
+        newUsername: newUserData.newUsername, // Get the new username from the newUserData state
+        newPassword: newUserData.newPassword, // Get the new password from the newUserData state
+      }),
+    });
+
+        //Conditional rendering to check if the response is successful
+    if (response.ok) { 
+      const data = await response.json(); // Parse the response body as JSON
+      // Conditional rendering to check if a token is present in the response data
+      if (data.token) { 
+        // Parse the existing users from local storage or initialise an empty array
+        const users = JSON.parse(localStorage.getItem('users')) || [];
+        // Create a new user object with username, password, and userId
+        const newUser = { username: newUserData.newUsername, password: newUserData.newPassword, userId: users.length + 1 };
+        users.push(newUser); // Add the new user object to the users array
+        localStorage.setItem('users', JSON.stringify(users)); // Store the updated users array in local storage
+      } else {
+        throw new Error('Invalid Server Response'); // Throw an error if the server response does not contain a token
+      }
+    } else {
+      const errorData = await response.json(); // Parse the response body as JSON
+      throw new Error(errorData.message); // Throw an error with the message from the server response
+      // throw new Error('Failed to add new user');
+    }
+  } catch (error) {
+    setError(`Error adding new user: ${error.message}`); // Set error message in state
   }
+};
 
   //Function to add new task
-  const addTask = async (newTask) => {
-    try {
-      const token = localStorage.getItem('token');
-      if (!token) {
-        throw new Error('No token available');
-      }
-      const response = await fetch('http://localhost:3001/users/addTask', {
-        method: 'POST',
-        headers: { 
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        },
-        body: JSON.stringify({user:newTask.user, title: newTask.title}),
-      });
-      if (response.ok) {
-        const newTaskObject = await response.json();
-        setTasks(tasks => [...tasks, newTaskObject]); 
-        console.log('Task added successfully');        
-      } 
-      else {
-        throw new Error('Failed to add task');
-      }
-    } catch (error) {
-      setError(`Error adding task: ${error.message}`);
+ const addTask = async (newTask) => {//Define an async function to add a new task
+  try {
+    const token = localStorage.getItem('token'); // Retrieve the authentication token from local storage
+    if (!token) {
+      throw new Error('No token available'); // Throw an error if no token is available
     }
-  };
+    
+    // Send a POST request to the addTask endpoint
+    const response = await fetch('http://localhost:3001/users/addTask', {
+      method: 'POST',
+      mode: 'cors',
+      headers: { 
+        'Content-Type': 'application/json', // Specify the content type as JSON
+        'Authorization': `Bearer ${token}`, // Include the token in the authorization header
+      },
+      body: JSON.stringify({ // Convert the new task data to a JSON string
+        user: newTask.user, // Get the user ID from the newTask object
+        title: newTask.title, // Get the task title from the newTask object
+      }),
+    });
+    
+    if (response.ok) { // Check if the response is successful
+      const newTaskObject = await response.json(); // Parse the response body as JSON
+      setTasks(tasks => [...tasks, newTaskObject]); // Add the new task to the tasks state
+      console.log('Task added successfully'); // Log a success message 
+    } else {
+      throw new Error('Failed to add task'); // Throw an error if the POST request is unsuccessful
+    }
+  } catch (error) {
+    setError(`Error adding task: ${error.message}`); // Set error message in state
+  }
+};
+
 
 
 
