621c832bb1f15efa0f93063fde787ff5d43a8a38
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-02-26 12:55:33 +0200

    Update users.js

--- TASK/simplified/routes/users.js
+++ TASK/simplified/routes/users.js
@@ -3,12 +3,7 @@ const express = require('express'); // Import Express.js framework
 const jwt = require('jsonwebtoken'); // Import JWT for token generation and verification
 const router = express.Router(); // Create a router instance
 const cors = require('cors'); // Import CORS middleware for cross-origin resource sharing
-// const { 
-//   checkJwtToken, 
-//   validateUsername, 
-//   limitTaskLength, 
-//   enforceContentType } 
-//   = require('./middleware');//Import middleware functions
+// const { checkJwtToken, validateUsername, limitTaskLength, enforceContentType } = require('./middleware');//Import middleware functions
 
 
 // Middleware to parse JSON bodies
@@ -30,6 +25,18 @@ let users = [
     password: 'passWord2',
   },
 ];
+// users = [
+//   {
+//     "id": "0",
+//     "username": "admin@gmail.com",
+//     "password": "passWord1"
+//   },
+//   {
+//     "id": 1,
+//     "username": "user1@gmail.com",
+//     "password": "passWord2"
+//   }
+// ]
 
 //----------TASKS DATA---------------------------
 // In-memory array used to store the data
@@ -45,11 +52,36 @@ let tasks = [
     title: "Implement custom middleware to authenticate user",
   },
 ];
+
+// [
+//   {
+//     "id": 0,
+//     "user": "admin@gmail.com",
+//     "title": "Implement a Post route for logging in"
+//   },
+//   {
+//     "id": 1,
+//     "user": "user1@gmail.com",
+//     "title": "Implement custom middleware to authenticate user"
+//   }
+// ]
+
+//=========ROUTES============
+
 // Route to send a GET for the root endpoint '/'
 router.get('/', (req, res, next) => {
   res.send('Respond with resource');
 });
 
+// router.get('/', (req, res) =>{
+//   try {
+//     res.send(tasks)
+//     console.log('Success message');
+
+//   } catch (error) {
+//     res.status(500).json('Internal Server Error')
+//   }
+// })
 
 // Route to send a GET request to the '/findTasks' endpoint
 router.get('/findTasks', /*checkJwtToken,*/ (req, res) => {
@@ -58,7 +90,12 @@ router.get('/findTasks', /*checkJwtToken,*/ (req, res) => {
   // Conditional rendering to check if taskId is provided in the request
   if (taskId) { 
     const task = tasks.find(task => task.id === parseInt(taskId)); // Find the task with the specified taskId
+    //parseInt()  is used to accept the string and radix parameter and convert it into an integer.
     return task ? res.json(task) : res.status(404).json({ message: 'Task not found' }); // Returns the task if found, or a 404 error if not found
+   /* task ?: Check if the variable task evaluates to true or false. If task is truthy(it exists), the condition is true.
+   res.json(task): If task exists, this part executes, sending a JSON response with the task details.
+ res.status(404).json({ message: 'Task not found' }): If task does not exist (i.e., the condition is false), it sets the HTTP status to 404 (Not Found) 
+ and sends a JSON response with a message indicating that the task was not found.*/
   } 
 
   res.json(tasks); // If no taskId is not provided, return all tasks
@@ -88,7 +125,8 @@ router.post("/login", (req, res) => {
     },
     'secretKey',//The secret key  (string used to sign the token)
     {
-      expiresIn: '12h' // Token expires in 12 hours
+      expiresIn: '12h',// Token expiration time
+      algorithm: 'HS256'
       //Options: optional object specifying additional settings for the token: This includes the expiration time for the token
     });
     res.json({ token: jwtToken }); // Send the token in the response
@@ -113,8 +151,11 @@ router.post('/register', /*validateUsername,*/ (req, res) => {
       return res.status(409).json({ message: 'Username is already taken' });// If username already exists, return a 409 Conflict response
     }
     else {
-      // If username is unique, generate a unique user ID and create a new user object
-      const newUserId = generateUniqueId(); // This function generates a unique user ID (not shown in the provided code)
+      // If username is unique, (the username does not yet already exist) generate a unique user ID and create a new user object
+      const newUserId = generateUniqueId(); // This function generates a unique user ID 
+      /* This identifier is typically used to uniquely identify each user in the system and can be helpful 
+      for various purposes such as database storage, referencing users in the application, or ensuring that no two users have the same identifier. */
+       // Create a new user object with the provided username, password, and generated ID
       const newUser = { id: newUserId, username: newUsername, password: newPassword };
 
       // Generate a JWT token for the new user
